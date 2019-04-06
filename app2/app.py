86a1a0a0e445d5fc82e3aa7aa4654b79b1261a55
Robert West <robert@theuglyhack.com>
2019-04-06 17:10:09 -0500

    updated network settings

--- app2/app.py
+++ app2/app.py
@@ -12,5 +12,5 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
