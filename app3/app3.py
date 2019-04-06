53b79d087cb955e4445854dc8f149e2e22d56df7
Robert West <robert@theuglyhack.com>
2019-04-06 12:39:08 -0500

    added test apps

---
+++ app3/app3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from flask import Flask,make_response, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "app3\n"
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
