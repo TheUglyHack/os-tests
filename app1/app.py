0e567ac1e56a513a557297d5e2d1cb37e6730f64
Robert West <robert@theuglyhack.com>
2019-04-07 11:07:07 -0500

    added oidc

--- app1/app.py
+++ app1/app.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 from flask import Flask,make_response, jsonify
+from flask_oidc import OpenIDConnect
 
 app = Flask(__name__)
+oidc = OpenIDConnect(app)
 
 @app.route('/')
 def index():
-    return "app1\n"
+    if oidc.user_loggedin:
+        return "app1: Welcome {}\n".format(oidc.user_getfield('email'))
+    else:
+        return "app1: not logged in\n"
+
+
+@app.route('/login')
+@oidc.require_login
+def login():
+    return "app1: Welcome %s\n" % oidc.user_getfield('email')
+
 
 @app.errorhandler(404)
 def not_found(error):
